5ea68404ee0da8923b554e33e127df6946635b36
Anthony-Shoshi <anthonyshoshigomes1996@gmail.com>
2025-06-09 23:32:03 +0200

    implemented apis for result page

--- src/app/procedure-results/page.tsx
+++ src/app/procedure-results/page.tsx
@@ -50,7 +50,6 @@ const ProcedureResultsPage = () => {
     setProcedureDetail(null);
   };
 
-  // Handle procedure change
   const handleProcedureChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedProcedureId(Number(e.target.value));
   };
