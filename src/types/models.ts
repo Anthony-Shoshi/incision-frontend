e242a3e043d6bed984e613403197b904baa1b394
Anthony-Shoshi <anthonyshoshigomes1996@gmail.com>
2025-06-15 06:26:04 +0200

    fixed type issue

--- src/types/models.ts
+++ src/types/models.ts
@@ -16,3 +16,39 @@ export type Surgeon = {
   speciality_id: number;
   created_at: string;
 }
+
+export type Material = {
+  material_name: string;
+  original_cost: number;
+  optimized_cost: number;
+};
+
+export type SurgeonBasic = {
+  id: number;
+  name: string;
+};
+
+export type ProcedureSummary = {
+  name: string;
+  original_cost: number;
+  optimized_cost: number;
+};
+
+export type MaterialItem = {
+  name: string;
+  original_price: number;
+  optimized_price: number;
+  surgeon_specific_action: string;
+};
+
+export type MaterialBreakdown = {
+  [materialType: string]: {
+    [subtype: string]: MaterialItem[];
+  };
+};
+
+export type ProcedureDetail = {
+  procedure: ProcedureSummary;
+  surgeons: SurgeonBasic[];
+  materials: MaterialBreakdown;
+};
